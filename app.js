0b743297ce1c235f67bb54f4e14bcca39cd8585f
Kacper Marciniak <kacper.d3veloper@gmail.com>
2023-01-03 19:14:08 +0100

    Contact me

--- app.js
+++ app.js
@@ -2,18 +2,49 @@
 const bodyParser = require('body-parser');
 const path = require("path");
 const express = require('express');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("node_modules"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("assets"));
+
+// contact 
+app.post('/contact', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        user: 'mailtransporter61@gmail.com',
+        port: 3000,
+        secure: true,
+        auth: {
+            type: 'login',
+            user: 'mailtransporter61@gmail.com',
+            pass: 'ogkayjwciwtcrpwc'
+        }
+    });
+    const mailOptions = {
+        from: 'mailtransporter61@gmail.com',
+        to: 'kacper5marciniak@gmail.com, kacper.d3veloper@gmail.com, ' + req.body.email, 
+        subject: 'Waiting for response',
+        text: 'Thanks for sending my your message! I will read it ASAP ;) Your mail: ' + req.body.email + ' Your message content: ' + req.body.message
+    }
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.redirect('/');
+});
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Portfolio" });
 });
-
 
 // server
 const hostname = '127.0.0.1';
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -6,11 +6,44 @@ var path = require("path");
 
 var express = require('express');
 
+var nodemailer = require('nodemailer');
+
 var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express["static"]("node_modules"));
-app.use(express["static"]("assets"));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+app.use(express["static"]("assets")); // contact 
+
+app.post('/contact', function (req, res) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    user: 'mailtransporter61@gmail.com',
+    port: 3000,
+    secure: true,
+    auth: {
+      type: 'login',
+      user: 'mailtransporter61@gmail.com',
+      pass: 'ogkayjwciwtcrpwc'
+    }
+  });
+  var mailOptions = {
+    from: 'mailtransporter61@gmail.com',
+    to: 'kacper5marciniak@gmail.com, kacper.d3veloper@gmail.com, ' + req.body.email,
+    subject: 'Waiting for response',
+    text: 'Thanks for sending my your message! I will read it ASAP ;) Your mail: ' + req.body.email + ' Your message content: ' + req.body.message
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.redirect('/');
+});
 app.get("/", function (req, res) {
   res.render("index", {
     title: "Portfolio"
